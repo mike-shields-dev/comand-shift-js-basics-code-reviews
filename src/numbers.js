8e3961e27b60a2f099faf035cb7c02e9a66152ae
Benj <ben.speight@outlook.com>
2023-02-22 21:13:19 +0000

    numbers bs

--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,45 @@
-function add (a, b) {
-  // your code here
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
 }
 
-function subtract (a, b) {
-  // your code here
+const multiply = (a, b) => {
+  return a * b;
 }
 
-function multiply (a, b) {
-  // your code here
+const divide = (a, b) => {
+  return a / b;
 }
 
-function divide (a, b) {
-  // your code here
+const power = (a, b) => {
+  return Math.pow (a, b);
 }
 
-function power (a, b) {
-  // your code here
+const round = (a) => {
+  return Math.round (a);
 }
 
-function round (a) {
-  // your code here
+const roundUp = (a) => {
+  return Math.ceil (a);
 }
 
-function roundUp (a) {
-  // your code here
+const roundDown = (a) => {
+  return Math.floor (a);
 }
 
-function roundDown (a) {
-  // your code here
+const absolute = (a) => {
+  return Math.abs(a);
 }
 
-function absolute (a) {
-  // your code here
+const quotient = (a, b) => {
+  return ~~(a/b);
 }
 
-function quotient (a, b) {
-  // your code here
-}
-
-function remainder (a, b) {
-  // your code here
+const remainder = (a, b) => {
+  return a % b;
 }
 
 module.exports = {
