dcb8bec98b366f82a2eeb0da1afb30355568aea7
Jose Wills <jose.wills3@gmail.com>
2023-02-23 21:25:41 +0000

    solved first 7 numbers tests

--- src/numbers.js
+++ src/numbers.js
@@ -1,29 +1,29 @@
 function add (a, b) {
-  // your code here
+  return a + b;
 }
 
 function subtract (a, b) {
-  // your code here
+  return a - b;
 }
 
 function multiply (a, b) {
-  // your code here
+  return a * b;
 }
 
 function divide (a, b) {
-  // your code here
+  return a / b;
 }
 
 function power (a, b) {
-  // your code here
+  return a ** b;
 }
 
 function round (a) {
-  // your code here
+  return Math.round(a);
 }
 
 function roundUp (a) {
-  // your code here
+  return Math.ceil(a);
 }
 
 function roundDown (a) {
