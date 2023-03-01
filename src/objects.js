dbeead7435cfc17a735800dfe4d7f3979fc02432
Benj <ben.speight@outlook.com>
2023-03-01 22:45:28 +0000

    objects

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,51 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+    name: name,
+    age: age
+  };
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return property in object;
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer === 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+  const totalAge = people.reduce((sum, person) => sum + person.age, 0);
+  return totalAge / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name: name,
+    age: age,
+    introduce: (friendName) => {
+      return `Hi ${friendName}, my name is ${name} and I am ${age}!`;
+    }
+  };
 };
 
 module.exports = {
