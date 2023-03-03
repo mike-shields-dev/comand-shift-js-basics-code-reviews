b7e3a78ddcaea84d5b5072225b6e5c969f9f35a5
AmyHMSmith <amyhmsmith@gmail.com>
2023-03-03 21:59:25 +0000

    booleans test

--- src/booleans.js
+++ src/booleans.js
@@ -1,62 +1,72 @@
 function negate(a) {
-  // your code here
-};
+  return !a;
+}
 
 function both(a, b) {
-  // your code here
-};
+  return a && b;
+}
 
 function either(a, b) {
-  // your code here
-};
+  return a || b;
+}
 
 function none(a, b) {
-  // your code here
-};
+  return !a && !b;
+}
 
 function one(a, b) {
-  // your code here
-};
+  return a !== b;
+  // return !(a === b);
+}
 
 function truthiness(a) {
-  // your code here
-};
+  console.log(a, Boolean (a));
+  return true && Boolean(a);
+}
 
 function isEqual(a, b) {
-  // your code here
-};
+  return a === b;
+}
 
 function isGreaterThan(a, b) {
-  // your code here
-};
+  return a > b;
+}
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
-};
+  return a <= b;
+}
 
 function isOdd(a) {
-  // your code here
-};
+  return a % 2 === 1;
+}
 
 function isEven(a) {
-  // your code here
-};
+  return a % 2 === 0;
+}
 
 function isSquare(a) {
-  // your code here
-};
+  return Number.isInteger(Math.sqrt(a));
+}
 
 function startsWith(char, string) {
-  // your code here
-};
+  return string.startsWith(char);
+}
 
 function containsVowels(string) {
-  // your code here
-};
+  const vowelRegex = /[aeiou]/gi;
+
+  const match = string.match(vowelRegex);
+  console.log(match,(match !== null));
+  return match !== null;
+}
 
 function isLowerCase(string) {
-  // your code here
-};
+
+  const ucaseRegex = /[A-Z]/;
+  const match = string.match(ucaseRegex);
+  console.log(match);
+  return match === null;
+}
 
 module.exports = {
   negate,
